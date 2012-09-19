0f3f213d707f0fcdf0bfcd33904d6ba4cd2e7010
xtalandier <xtalandier@ITEM-58159.dhcp.aix2.fr.sopra>
2012-09-19 12:13:58 +0200

    Implementing method and colors

---
+++ info-repartie/src/info/repartie/Color.java
@@ -0,0 +1,32 @@
+package info.repartie;
+
+/**
+ * Define color
+ * @author xtalandier
+ */
+public class Color {
+	/**
+	 * Color RED (#FF0000)
+	 */
+	public static final String RED   = "red";
+
+	/**
+	 * Color GREEN (#00FF00)
+	 */
+	public static final String GREEN = "green";
+
+	/**
+	 * Color BLUE (#0000FF)
+	 */
+	public static final String BLUE  = "blue";
+
+	/**
+	 * Color BLACK (#000000)
+	 */
+	public static final String BLACK  = "black";
+
+	/**
+	 * Color WHITE (#FFFFFF)
+	 */
+	public static final String WHITE  = "white";
+}
