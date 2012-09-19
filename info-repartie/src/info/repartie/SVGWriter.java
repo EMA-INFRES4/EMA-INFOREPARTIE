5ec0c6c4a9b9edde49dacc3da5586b0307128ed7
cassaire <cyril_rubini@yahoo.fr>
2012-09-19 11:26:02 +0200

    Add class SVGWriter.java

---
+++ info-repartie/src/info/repartie/SVGWriter.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.repartie;
+
+/**
+ *
+ * @author Cyril
+ */
+public class SVGWriter {
+	
+}
